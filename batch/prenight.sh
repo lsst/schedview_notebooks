420a7794aa3834517cc4bb622f6bf532c3e2e8f2
Eric Neilsen <neilsen@fnal.gov>
2025-07-01 09:08:10 -0700

    close incomplete if

--- batch/prenight.sh
+++ batch/prenight.sh
@@ -43,6 +43,7 @@ else
   if [[ ! ${SCHEDVIEW_DAY_OBS} =~ ^20[0-9]{6}$ ]]; then
     echo "SCHEDVIEW_DAY_OBS must by in YYYYMMDD format"
     exit 1
+  fi
 fi
 DAYOBS_YY=$(echo $SCHEDVIEW_DAY_OBS | cut -c-4)
 DAYOBS_MM=$(echo $SCHEDVIEW_DAY_OBS | cut -c5-6)
@@ -54,78 +55,78 @@ if [-z ${SCHEDVIEW_INSTRUMENTS+xxx} ] ; then
 fi
 
 for SCHEDVIEW_INSTRUMENT in ${SCHEDVIEW_INSTRUMENTS} ; do
-    if [ ${SCHEDVIEW_INSTRUMENT} == "lsstcam" ] ; then SCHEDVIEW_TELESCOPE="simonyi" ; else SCHEDVIEW_TELESCOPE="auxtel" ; fi
-    export SCHEDVIEW_INSTRUMENT
-    export SCHEDVIEW_TELESCOPE
+  if [ ${SCHEDVIEW_INSTRUMENT} == "lsstcam" ] ; then SCHEDVIEW_TELESCOPE="simonyi" ; else SCHEDVIEW_TELESCOPE="auxtel" ; fi
+  export SCHEDVIEW_INSTRUMENT
+  export SCHEDVIEW_TELESCOPE
 
-    IFS=' ' read SCHEDVIEW_SIM_DATE SCHEDVIEW_SIM_INDEX <<< $(prenight_inventory ${SCHEDVIEW_DAY_OBS} | awk -F "\t" '$4~/'${SCHEDVIEW_TELESCOPE}'/ && $8~/.*ideal.*/ && $8~/.*nominal.*/ {print $2, $3}' )
-    export SCHEDVIEW_SIM_DATE
-    export SCHEDVIEW_SIM_INDEX
+  IFS=' ' read SCHEDVIEW_SIM_DATE SCHEDVIEW_SIM_INDEX <<< $(prenight_inventory ${SCHEDVIEW_DAY_OBS} | awk -F "\t" '$4~/'${SCHEDVIEW_TELESCOPE}'/ && $8~/.*ideal.*/ && $8~/.*nominal.*/ {print $2, $3}' )
+  export SCHEDVIEW_SIM_DATE
+  export SCHEDVIEW_SIM_INDEX
 
-    echo "Preparing prenight directory for this prenight"
-    date --iso=s
-    # Make the directory in which to work and save the html file
-    PRENIGHT_DIR="/sdf/data/rubin/shared/scheduler/reports/prenight/${SCHEDVIEW_INSTRUMENT}/${DAYOBS_YY}/${DAYOBS_MM}/${DAYOBS_DD}"
-    mkdir -p ${PRENIGHT_DIR}
-    cd ${PRENIGHT_DIR}
+  echo "Preparing prenight directory for this prenight"
+  date --iso=s
+  # Make the directory in which to work and save the html file
+  PRENIGHT_DIR="/sdf/data/rubin/shared/scheduler/reports/prenight/${SCHEDVIEW_INSTRUMENT}/${DAYOBS_YY}/${DAYOBS_MM}/${DAYOBS_DD}"
+  mkdir -p ${PRENIGHT_DIR}
+  cd ${PRENIGHT_DIR}
 
-    PRENIGHT_SOURCE="/sdf/data/rubin/user/neilsen/forcron/schedview_notebooks/prenight/prenight.ipynb"
-    echo "Copying prenight.ipynb from ${PRENIGHT_SOURCE}"
-    date --iso=s
-    # Get the notebook
-    PRENIGHT_FNAME_BASE="prenight_${DAYOBS_YY}-${DAYOBS_MM}-${DAYOBS_DD}"
-    PRENIGHT_FNAME=${PRENIGHT_FNAME_BASE}.ipynb
-    # Do not just blindly check out of git, but copy from somewhere hand
-    # checked.
-    cp ${PRENIGHT_SOURCE} $PRENIGHT_FNAME
+  PRENIGHT_SOURCE="/sdf/data/rubin/user/neilsen/forcron/schedview_notebooks/prenight/prenight.ipynb"
+  echo "Copying prenight.ipynb from ${PRENIGHT_SOURCE}"
+  date --iso=s
+  # Get the notebook
+  PRENIGHT_FNAME_BASE="prenight_${DAYOBS_YY}-${DAYOBS_MM}-${DAYOBS_DD}"
+  PRENIGHT_FNAME=${PRENIGHT_FNAME_BASE}.ipynb
+  # Do not just blindly check out of git, but copy from somewhere hand
+  # checked.
+  cp ${PRENIGHT_SOURCE} $PRENIGHT_FNAME
 
-    echo "Executing the prenight notebook"
-    date --iso=s
-    # At the USDF, the timings can be very slow compared to running on a laptop,
-    # so set the timeouts really high.
-    # Setting the kernel_name to python3 uses the kernel from the activated
-    # conda environment.
-    time jupyter nbconvert \
-        --to html \
-        --execute \
-        --no-input \
-        --ExecutePreprocessor.kernel_name=python3 \
-        --ExecutePreprocessor.startup_timeout=3600 \
-        --ExecutePreprocessor.timeout=3600 \
-        ${PRENIGHT_FNAME}
+  echo "Executing the prenight notebook"
+  date --iso=s
+  # At the USDF, the timings can be very slow compared to running on a laptop,
+  # so set the timeouts really high.
+  # Setting the kernel_name to python3 uses the kernel from the activated
+  # conda environment.
+  time jupyter nbconvert \
+      --to html \
+      --execute \
+      --no-input \
+      --ExecutePreprocessor.kernel_name=python3 \
+      --ExecutePreprocessor.startup_timeout=3600 \
+      --ExecutePreprocessor.timeout=3600 \
+      ${PRENIGHT_FNAME}
 
 
-    echo "Preparing multiprenight directory for this dayobs"
-    date --iso=s
-    # Make the directory in which to work and save the html file
-    MULTIPRENIGHT_DIR="/sdf/data/rubin/shared/scheduler/reports/multiprenight/${SCHEDVIEW_INSTRUMENT}/${DAYOBS_YY}/${DAYOBS_MM}/${DAYOBS_DD}"
-    mkdir -p ${MULTIPRENIGHT_DIR}
-    cd ${MULTIPRENIGHT_DIR}
+  echo "Preparing multiprenight directory for this dayobs"
+  date --iso=s
+  # Make the directory in which to work and save the html file
+  MULTIPRENIGHT_DIR="/sdf/data/rubin/shared/scheduler/reports/multiprenight/${SCHEDVIEW_INSTRUMENT}/${DAYOBS_YY}/${DAYOBS_MM}/${DAYOBS_DD}"
+  mkdir -p ${MULTIPRENIGHT_DIR}
+  cd ${MULTIPRENIGHT_DIR}
 
-    MULTIPRENIGHT_SOURCE="/sdf/data/rubin/user/neilsen/forcron/schedview_notebooks/prenight/multiprenight.ipynb"
-    echo "Copying multiprenight.ipynb from ${MULTIPRENIGHT_SOURCE}"
-    date --iso=s
-    # Get the notebook
-    MULTIPRENIGHT_FNAME_BASE="multiprenight_${DAYOBS_YY}-${DAYOBS_MM}-${DAYOBS_DD}"
-    MULTIPRENIGHT_FNAME=${MULTIPRENIGHT_FNAME_BASE}.ipynb
-    # Do not just blindly check out of git, but copy from somewhere hand
-    # checked.
-    cp ${MULTIPRENIGHT_SOURCE} $MULTIPRENIGHT_FNAME
+  MULTIPRENIGHT_SOURCE="/sdf/data/rubin/user/neilsen/forcron/schedview_notebooks/prenight/multiprenight.ipynb"
+  echo "Copying multiprenight.ipynb from ${MULTIPRENIGHT_SOURCE}"
+  date --iso=s
+  # Get the notebook
+  MULTIPRENIGHT_FNAME_BASE="multiprenight_${DAYOBS_YY}-${DAYOBS_MM}-${DAYOBS_DD}"
+  MULTIPRENIGHT_FNAME=${MULTIPRENIGHT_FNAME_BASE}.ipynb
+  # Do not just blindly check out of git, but copy from somewhere hand
+  # checked.
+  cp ${MULTIPRENIGHT_SOURCE} $MULTIPRENIGHT_FNAME
 
-    echo "Executing the multiprenight notebook"
-    date --iso=s
-    # At the USDF, the timings can be very slow compared to running on a laptop,
-    # so set the timeouts really high.
-    # Setting the kernel_name to python3 uses the kernel from the activated
-    # conda environment.
-    time jupyter nbconvert \
-        --to html \
-        --execute \
-        --no-input \
-        --ExecutePreprocessor.kernel_name=python3 \
-        --ExecutePreprocessor.startup_timeout=3600 \
-        --ExecutePreprocessor.timeout=3600 \
-        ${MULTIPRENIGHT_FNAME}
+  echo "Executing the multiprenight notebook"
+  date --iso=s
+  # At the USDF, the timings can be very slow compared to running on a laptop,
+  # so set the timeouts really high.
+  # Setting the kernel_name to python3 uses the kernel from the activated
+  # conda environment.
+  time jupyter nbconvert \
+      --to html \
+      --execute \
+      --no-input \
+      --ExecutePreprocessor.kernel_name=python3 \
+      --ExecutePreprocessor.startup_timeout=3600 \
+      --ExecutePreprocessor.timeout=3600 \
+      ${MULTIPRENIGHT_FNAME}
 
 done
 
--- batch/scheduler_nightsum.sh
+++ batch/scheduler_nightsum.sh
@@ -42,6 +42,7 @@ else
   if [[ ! ${SCHEDVIEW_DAY_OBS} =~ ^20[0-9]{6}$ ]]; then
     echo "SCHEDVIEW_DAY_OBS must by in YYYYMMDD format"
     exit 1
+  fi
 fi
 DAYOBS_YY=$(echo $SCHEDVIEW_DAY_OBS | cut -c-4)
 DAYOBS_MM=$(echo $SCHEDVIEW_DAY_OBS | cut -c5-6)
@@ -53,74 +54,73 @@ if [-z ${SCHEDVIEW_INSTRUMENTS+xxx} ] ; then
 fi
 
 for SCHEDVIEW_VISIT_ORIGIN in ${SCHEDVIEW_INSTRUMENTS} ; do
-    export SCHEDVIEW_VISIT_ORIGIN
+  export SCHEDVIEW_VISIT_ORIGIN
 
-    echo "Preparing directory for this nightsum"
-    date --iso=s
-    # Make the directory in which to work and save the html file
-    NIGHTSUM_DIR="/sdf/data/rubin/shared/scheduler/reports/nightsum/${SCHEDVIEW_VISIT_ORIGIN}/${DAYOBS_YY}/${DAYOBS_MM}/${DAYOBS_DD}"
-    mkdir -p ${NIGHTSUM_DIR}
-    cd ${NIGHTSUM_DIR}
+  echo "Preparing directory for this nightsum"
+  date --iso=s
+  # Make the directory in which to work and save the html file
+  NIGHTSUM_DIR="/sdf/data/rubin/shared/scheduler/reports/nightsum/${SCHEDVIEW_VISIT_ORIGIN}/${DAYOBS_YY}/${DAYOBS_MM}/${DAYOBS_DD}"
+  mkdir -p ${NIGHTSUM_DIR}
+  cd ${NIGHTSUM_DIR}
 
-    SCHEDULER_NIGHTSUM_SOURCE="/sdf/data/rubin/user/neilsen/forcron/schedview_notebooks/nightly/scheduler-nightsum.ipynb"
-    echo "Copying scheduler_nightsum.ipynb from ${SCHEDULER_NIGHTSUM_SOURCE}"
-    date --iso=s
-    # Get the notebook
-    NIGHTSUM_FNAME_BASE="nightsum_${DAYOBS_YY}-${DAYOBS_MM}-${DAYOBS_DD}"
-    NIGHTSUM_FNAME=${NIGHTSUM_FNAME_BASE}.ipynb
-    # Do not just blindly check out of git, but copy from somewhere hand
-    # checked.
-    cp ${SCHEDULER_NIGHTSUM_SOURCE} $NIGHTSUM_FNAME
+  SCHEDULER_NIGHTSUM_SOURCE="/sdf/data/rubin/user/neilsen/forcron/schedview_notebooks/nightly/scheduler-nightsum.ipynb"
+  echo "Copying scheduler_nightsum.ipynb from ${SCHEDULER_NIGHTSUM_SOURCE}"
+  date --iso=s
+  # Get the notebook
+  NIGHTSUM_FNAME_BASE="nightsum_${DAYOBS_YY}-${DAYOBS_MM}-${DAYOBS_DD}"
+  NIGHTSUM_FNAME=${NIGHTSUM_FNAME_BASE}.ipynb
+  # Do not just blindly check out of git, but copy from somewhere hand
+  # checked.
+  cp ${SCHEDULER_NIGHTSUM_SOURCE} $NIGHTSUM_FNAME
 
-    echo "Executing the nightsum notebook"
-    date --iso=s
-    # At the USDF, the timings can be very slow compared to running on a laptop,
-    # so set the timeouts really high.
-    # Setting the kernel_name to python3 uses the kernel from the activated
-    # conda environment.
-    time jupyter nbconvert \
-        --to html \
-        --execute \
-        --no-input \
-        --ExecutePreprocessor.kernel_name=python3 \
-        --ExecutePreprocessor.startup_timeout=3600 \
-        --ExecutePreprocessor.timeout=3600 \
-        ${NIGHTSUM_FNAME}
-
-
-    echo "Preparing directory for this prenight comparison"
-    date --iso=s
-    # Make the directory in which to work and save the html file
-    COMPARE_NIGHT_DIR="/sdf/data/rubin/shared/scheduler/reports/compareprenight/${SCHEDVIEW_VISIT_ORIGIN}/${DAYOBS_YY}/${DAYOBS_MM}/${DAYOBS_DD}"
-    mkdir -p ${COMPARE_NIGHT_DIR}
-    cd ${COMPARE_NIGHT_DIR}
-
-    COMPARE_NIGHT_SOURCE="/sdf/data/rubin/user/neilsen/forcron/schedview_notebooks/nightly/compareprenight.ipynb"
-    echo "Copying compareprenight.ipynb from ${COMPARE_NIGHT_SOURCE}"
-    date --iso=s
-    # Get the notebook
-    COMPARE_NIGHT_FNAME_BASE="comparenight_${DAYOBS_YY}-${DAYOBS_MM}-${DAYOBS_DD}"
-    COMPARE_NIGHT_FNAME=${COMPARE_NIGHT_FNAME_BASE}.ipynb
-    # Do not just blindly check out of git, but copy from somewhere hand
-    # checked.
-    cp ${COMPARE_NIGHT_SOURCE} $COMPARE_NIGHT_FNAME
+  echo "Executing the nightsum notebook"
+  date --iso=s
+  # At the USDF, the timings can be very slow compared to running on a laptop,
+  # so set the timeouts really high.
+  # Setting the kernel_name to python3 uses the kernel from the activated
+  # conda environment.
+  time jupyter nbconvert \
+      --to html \
+      --execute \
+      --no-input \
+      --ExecutePreprocessor.kernel_name=python3 \
+      --ExecutePreprocessor.startup_timeout=3600 \
+      --ExecutePreprocessor.timeout=3600 \
+      ${NIGHTSUM_FNAME}
 
 
-    echo "Executing the compare prenight notebook"
-    date --iso=s
-    # At the USDF, the timings can be very slow compared to running on a laptop,
-    # so set the timeouts really high.
-    # Setting the kernel_name to python3 uses the kernel from the activated
-    # conda environment.
-    time jupyter nbconvert \
-        --to html \
-        --execute \
-        --no-input \
-        --ExecutePreprocessor.kernel_name=python3 \
-        --ExecutePreprocessor.startup_timeout=3600 \
-        --ExecutePreprocessor.timeout=3600 \
-        ${COMPARE_NIGHT_FNAME}
+  echo "Preparing directory for this prenight comparison"
+  date --iso=s
+  # Make the directory in which to work and save the html file
+  COMPARE_NIGHT_DIR="/sdf/data/rubin/shared/scheduler/reports/compareprenight/${SCHEDVIEW_VISIT_ORIGIN}/${DAYOBS_YY}/${DAYOBS_MM}/${DAYOBS_DD}"
+  mkdir -p ${COMPARE_NIGHT_DIR}
+  cd ${COMPARE_NIGHT_DIR}
 
+  COMPARE_NIGHT_SOURCE="/sdf/data/rubin/user/neilsen/forcron/schedview_notebooks/nightly/compareprenight.ipynb"
+  echo "Copying compareprenight.ipynb from ${COMPARE_NIGHT_SOURCE}"
+  date --iso=s
+  # Get the notebook
+  COMPARE_NIGHT_FNAME_BASE="comparenight_${DAYOBS_YY}-${DAYOBS_MM}-${DAYOBS_DD}"
+  COMPARE_NIGHT_FNAME=${COMPARE_NIGHT_FNAME_BASE}.ipynb
+  # Do not just blindly check out of git, but copy from somewhere hand
+  # checked.
+  cp ${COMPARE_NIGHT_SOURCE} $COMPARE_NIGHT_FNAME
+
+
+  echo "Executing the compare prenight notebook"
+  date --iso=s
+  # At the USDF, the timings can be very slow compared to running on a laptop,
+  # so set the timeouts really high.
+  # Setting the kernel_name to python3 uses the kernel from the activated
+  # conda environment.
+  time jupyter nbconvert \
+      --to html \
+      --execute \
+      --no-input \
+      --ExecutePreprocessor.kernel_name=python3 \
+      --ExecutePreprocessor.startup_timeout=3600 \
+      --ExecutePreprocessor.timeout=3600 \
+      ${COMPARE_NIGHT_FNAME}
 done
 
 echo "Rebuilding schedview report table of contents"
